67bf55b63364cde81207bc8b3f6c5e5d596ca1bd
vanrep <vanjarepnik87@gmail.com>
2026-04-08 21:58:23 +0200

    Test commit

--- src/main/java/eventease/DemoApplication.java
+++ src/main/java/eventease/DemoApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		// This is a test comment
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
