34dcce6767bbf495cbf979a8b18c75faeb0c352b
vanrep <vanjarepnik87@gmail.com>
2026-04-16 22:09:48 +0200

    Cambio de auth

---
+++ src/main/java/eventease/Dto/LoginRequestDto.java
@@ -0,0 +1,26 @@
+package eventease.Dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequestDto {
+   
+    // email del usuario para iniciar sesión
+    @NotBlank(message = "El email es obligatorio")
+    @Email(message = "El email no es válido")
+    private String email;
+
+    // contraseña del usuario
+    @NotBlank(message = "La contraseña es obligatoria")
+    private String password;
+   
+}
---
+++ src/main/java/eventease/Service/JSONWebTokenService.java
@@ -0,0 +1,59 @@
+package eventease.Service;
+
+import java.security.Key;
+import java.util.Date;
+
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+@Service
+public class JSONWebTokenService {
+    
+    // clave secreta 
+    private final String SECRET = "paella_valenciana";
+
+    // generar token
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email) // guarda el email dentro del token
+                .setIssuedAt(new Date()) // fecha de creación
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1 hora
+                .signWith(getKey()) // firma el token
+                .compact();
+    }
+
+    // extraer email del token
+    public String extractEmail(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    // validar token
+    public boolean isTokenValid(String token, String email) {
+        final String emailToken = extractEmail(token);
+        return (emailToken.equals(email) && !isTokenExpired(token));
+    }
+
+    // comprobar si expiró
+    private boolean isTokenExpired(String token) {
+        return extractAllClaims(token).getExpiration().before(new Date());
+    }
+
+    // leer contenido del token
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    // generar clave
+    private Key getKey() {
+        return Keys.hmacShaKeyFor(SECRET.getBytes());
+    }
+
+
+}
